621792a890acb2ace38078db9757f5123757f329
mahak <mahakj@valyoo.in>
2016-10-15 01:19:50 +0530

    crud added

--- model.php
+++ model.php
@@ -6,6 +6,8 @@ $database = readline("name the database: ");
 $table = readline("name the table name for which you want to create hibernate model: ");
 $db2 = mysql_pconnect($server_ip,$server_username,$server_password,false);
         mysql_select_db($database,$db2) or die(mysql_error());
+createModel($table, $db2);
+function createModel($table, $db2) {
 $re = mysql_query("show create table ".$table,$db2);
 $row = mysql_fetch_row($re);
 //print_r($row['1']);
@@ -41,8 +43,33 @@ $primary_key = str_replace("(`","",$primary_key);
 //print_r($primary_key);
 //var_dump($columnArray);
 
+$keyArray = Array();
+$j = 0;
+//print_r($arr);
+//die();
+while($arr1['2'] != 'CONSTRAINT' && $arr1['2'] != 'ENGINE'){
+	if($arr1['2'] == 'PRIMARY'){
+		$keyArrayTemp = str_replace("`),","",$arr1['4']);
+		$keyArrayTemp = str_replace("(`","",$keyArrayTemp);
+		$keyArray[str_replace("`","",$keyArrayTemp)] = "UNIQUE";
+	} else if($arr1['2'] == 'UNIQUE') {
+		$keyArrayTemp = str_replace("`),","",$arr1['5']);
+		$keyArrayTemp = str_replace("(`","",$keyArrayTemp);
+		$keyArray[str_replace("`","",$keyArrayTemp)] = "UNIQUE";
+	} else if($arr1['2'] == 'KEY') {
+		$keyArrayTemp = str_replace("`),","",$arr1['4']);
+		$keyArrayTemp = str_replace("(`","",$keyArrayTemp);
+		if(!array_key_exists(str_replace("`","",$keyArrayTemp), $keyArray)) {
+			$keyArray[str_replace("`","",$keyArrayTemp)] = 1;
+		}
+	}
+	$arr1 = explode(" ", $arr[$i]);
+	$i++;
+	$j++;
+}
 
-$model_header = "import java.io.Serializable;\n";
+
+$model_header = "package com.mac.model;\n\nimport java.io.Serializable;\n";
 if(in_array("Timestamp",$columnArray)){
 $model_header.= "import java.sql.Timestamp;\n";
 }
@@ -50,6 +77,18 @@ $model_header.= "\nimport javax.persistence.Column;\nimport javax.persistence.En
 
 
 $model_footer = "}";
+
+
+$repository_header = "package com.mac.dao;\n\nimport java.util.List;\n";
+if(in_array("Timestamp",$columnArray)){
+$repository_header.= "import java.sql.Timestamp;\n";
+}
+
+$repository_header.= "\nimport org.springframework.data.repository.CrudRepository;\nimport com.mac.model.".snakeToCamelCase($table_name,true).";\n\npublic interface " . snakeToCamelCase($table_name,true) . "Repository extends CrudRepository<".snakeToCamelCase($table_name,true).",".$columnArray[$primary_key].">{\n";
+
+
+
+$repository_footer = "}";
 
 
 $variable = "";
@@ -72,20 +111,58 @@ foreach($columnArray as $x => $x_value) {
     $seter.= "\tpublic void set" . snakeToCamelCase($x,true) . "(".$x_value." ".snakeToCamelCase($x).") {\n\t\tthis.".snakeToCamelCase($x)." = ".snakeToCamelCase($x).";\n\t}\n";
 }
 
+//print_r($keyArray);
+$finder = "";
+foreach($keyArray as $x => $x_value) {
+    $subKeyArray = explode(",",$x);
+	//print_r($subKeyArray);die();
+    $capatalizeNonTypeString = "";
+    $noncapatalizeWithTypeString = "";
+    foreach($subKeyArray as $y => $y_value){
+	//echo $y;
+	//echo $y_value;
+	if($capatalizeNonTypeString != ""){
+		$capatalizeNonTypeString.= "And";
+	}
+	$capatalizeNonTypeString.= snakeToCamelCase($y_value,true);
+	if($noncapatalizeWithTypeString != ""){
+		$noncapatalizeWithTypeString.= ", ";
+	}
+	$noncapatalizeWithTypeString.= $columnArray[$y_value]. " " .snakeToCamelCase($y_value);
+    }
+    if($x_value == "UNIQUE"){
+	$finder.= "\tpublic ".snakeToCamelCase($table_name,true)." findOneBy" . $capatalizeNonTypeString . "(" . $noncapatalizeWithTypeString . ");\n";
+    } else {
+    	$finder.= "\tpublic List<".snakeToCamelCase($table_name,true)."> findAllBy" . $capatalizeNonTypeString . "(" . $noncapatalizeWithTypeString . ");\n";
+    }
+}
+
+//echo $finder;
+//die();
+
 //echo $model_header;
 //echo $variable;
 //echo $geter;
 //echo $seter;
 //echo $model_footer;
 
-$file = fopen(snakeToCamelCase($table_name,true).".java", "w") or die("Unable to open file!");
+$fileModel = fopen(snakeToCamelCase($table_name,true).".java", "w") or die("Unable to open file!");
 $txt = $model_header;
 $txt .= $variable;
 $txt .= $geter;
 $txt .= $seter;
 $txt .= $model_footer;
-fwrite($file, $txt);
-fclose($file);
+fwrite($fileModel, $txt);
+fclose($fileModel);
+
+
+$fileRepo = fopen(snakeToCamelCase($table_name,true)."Repository.java", "w") or die("Unable to open file!");
+$txt = $repository_header;
+$txt .= $finder;
+$txt .= $repository_footer;
+fwrite($fileRepo, $txt);
+fclose($fileRepo);
+}
 
 
 function snakeToCamelCase($string, $capitalizeFirstCharacter = false) 
